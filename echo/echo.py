3c113412df12325b0fc3aceea3f9b4496163ea46
theahmedmuneeb <101744347+theahmedmuneeb@users.noreply.github.com>
2024-07-22 13:35:11 +0500

    Update echo.py

--- echo/echo.py
+++ echo/echo.py
@@ -1,6 +1,5 @@
 import discord
 from redbot.core import commands
-import re
 
 class Echo(commands.Cog):
     """A cog for echoing messages to specified channels."""
@@ -11,47 +10,38 @@ class Echo(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def echo(self, ctx, channel_input: str = None, message_id: int = None, *, message: str):
-        """Echo a message to a specified channel or reply to a message.
-        
-        Usage: -echo [- | <channel_id> | #channel | [{channel_id}]] [<message_id>] <message>
-        """
+        """Echo a message to a specified channel or reply to a message."""
         if channel_input == "-":
             channel = ctx.channel
         else:
             channel = await self.get_channel(ctx, channel_input)
 
-        # Format the message to replace custom emoji syntax
-        formatted_message = self.format_message(message)
-
         if not channel and message_id:
             try:
                 msg = await ctx.channel.fetch_message(message_id)
-                await msg.reply(formatted_message, allowed_mentions=discord.AllowedMentions(everyone=True, roles=True))
+                await msg.reply(message, allowed_mentions=discord.AllowedMentions(everyone=True, roles=True))
                 return
             except discord.NotFound:
                 pass
 
-        if not channel and formatted_message:
-            await ctx.send(formatted_message, allowed_mentions=discord.AllowedMentions(everyone=True, roles=True))
+        if not channel and message:
+            await ctx.send(message, allowed_mentions=discord.AllowedMentions(everyone=True, roles=True))
         elif channel and message_id:
             try:
                 msg = await channel.fetch_message(message_id)
-                await msg.reply(formatted_message, allowed_mentions=discord.AllowedMentions(everyone=True, roles=True))
+                await msg.reply(message, allowed_mentions=discord.AllowedMentions(everyone=True, roles=True))
             except discord.NotFound:
                 await ctx.send(f"Message with ID {message_id} not found in {channel.mention}. Sending message to {channel.mention}.")
-                await channel.send(formatted_message, allowed_mentions=discord.AllowedMentions(everyone=True, roles=True))
+                await channel.send(message, allowed_mentions=discord.AllowedMentions(everyone=True, roles=True))
         elif channel:
-            await channel.send(formatted_message, allowed_mentions=discord.AllowedMentions(everyone=True, roles=True))
+            await channel.send(message, allowed_mentions=discord.AllowedMentions(everyone=True, roles=True))
         else:
-            await ctx.send("Invalid usage. Use `-echo [- | <channel_id> | #channel | [{channel_id}]] [<message_id>] <message>`.")
+            await ctx.send("Invalid usage. Use -echo [- | <channel_id> | #channel] [<message_id>] <message>.")
 
     async def get_channel(self, ctx, channel_input):
         """Helper function to get channel object from input."""
         if channel_input.startswith("<#") and channel_input.endswith(">"):
             channel_id = int(channel_input[2:-1])
-            channel = ctx.guild.get_channel(channel_id)
-        elif channel_input.startswith("[{") and channel_input.endswith("}]"):
-            channel_id = int(channel_input[2:-2])
             channel = ctx.guild.get_channel(channel_id)
         elif channel_input.isdigit():
             try:
@@ -63,17 +53,6 @@ class Echo(commands.Cog):
             channel = None
         
         return channel
-
-    def format_message(self, message):
-        """Helper function to format custom emoji syntax in the message."""
-        # Replace [{;emojiname; emojiid}] with <:emojiname:emojiid>
-        custom_emoji_pattern = re.compile(r"\[\{;(.*?);(\d+);}]")
-        formatted_message = custom_emoji_pattern.sub(r"<:\1:\2>", message)
-        
-        print(f"Original message: {message}")  # Debugging line
-        print(f"Formatted message: {formatted_message}")  # Debugging line
-        
-        return formatted_message
 
     @echo.error
     async def echo_error(self, ctx, error):
